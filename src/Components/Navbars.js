92ce1f6f6aac7b3315d495be74afe8906f24c0ff
zakisamson <zakisamson@gmail.com>
2021-05-11 11:20:29 +0700

    Moving folder

---
+++ src/Components/Navbars.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Logo from '../Assets/zaki.png'
+import './Navbars.css'
+
+const Navbars = () => {
+  return (
+    <div className="Navbars">
+      <nav className="">
+        
+      </nav>
+    </div>
+  );
+}
+
+export default Navbars;
